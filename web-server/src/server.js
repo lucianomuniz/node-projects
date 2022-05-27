20196753ba1edad93e489f9a9cac52e69c66bba7
Luciano Muniz <luciano.mo@gmail.com>
2022-05-27 14:53:53 -0300

    added http server and html hbs templates

--- web-server/src/server.js
+++ web-server/src/server.js
@@ -1,14 +1,43 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
 
 const app = express();
-const publicDirPath = path.join(__dirname, '/public');
 
+// Define paths for Express config
+const publicDirPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDirPath));
 
-app.get('', (req, res) => {
-  res.render('index');
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Robot Tech',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Robot Tech',
+    image: '/img/robot.png',
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    name: 'Robot Tech',
+    helpMessage: 'Press F1 if you have any question.',
+  });
 });
 
 app.get('/weather', (req, res) => {
@@ -17,6 +46,22 @@ app.get('/weather', (req, res) => {
     temperature: 0,
     feelslike: -2,
     preciptation: 50,
+  });
+});
+
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Robot Tech',
+    errorMessage: 'Help article not found',
+  });
+});
+
+app.get('/*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Robot Tech',
+    errorMessage: 'Page not found',
   });
 });
 
