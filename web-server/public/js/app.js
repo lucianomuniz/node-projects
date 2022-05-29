2b2b6dfdaa19ca7b340fe9ca3df3afed96a36948
Luciano Muniz <luciano.mo@gmail.com>
2022-05-29 19:24:39 -0300

    added client side functionality

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,42 @@
-console.log('client side javascript file is loaded!');
+// fetch(url)
+//   .then((response) => {
+//     response.json().then((data) => {
+//       console.log(data);
+//     });
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+const getFetch = async (url) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-one');
+const messageTwo = document.querySelector('#message-two');
+
+messageOne.textContent = 'Loading...';
+messageOne.textContent = '';
+
+weatherForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const location = search.value;
+  const url = `http://localhost:3000/weather?address=${location}`;
+  const data = await getFetch(url);
+  if (data.error) {
+    messageOne.textContent = data.error;
+    messageTwo.textContent = '';
+  } else {
+    messageOne.textContent = data.location;
+    messageTwo.textContent = data.forecast;
+    search.value = '';
+  }
+});
